ce85dd72cd8e9c318b4daf10457f2c17cffd9a98
TamaraWG <walkertamara32@gmail.com>
2023-05-21 00:39:08 -0500

    added JavaScript  elements

--- src/script.js
+++ src/script.js
@@ -1,8 +1,14 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let conditionsElement = document.querySelector("#conditions");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  conditionsElement.innerHTML = response.data.weather[0].conditions;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "0f77edb1aff98694c739634ccdcf1f06";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&apiKey=${apiKey}&units=metric`;
