ef34997549f6ad5533670d52a55a827cf41a5009
TamaraWG <walkertamara32@gmail.com>
2023-05-21 13:45:33 -0500

    changed conditions to description

--- src/script.js
+++ src/script.js
@@ -32,18 +32,20 @@ function formatDay(timestamp) {
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let conditionsElement = document.querySelector("#conditions");
+  let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  conditionsElement.innerHTML = response.data.weather[0].conditions;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "0f77edb1aff98694c739634ccdcf1f06";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&apiKey=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
