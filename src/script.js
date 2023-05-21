eab489c8cbb30316427ab7d791ce7e821f598149
TamaraWG <walkertamara32@gmail.com>
2023-05-21 15:20:06 -0500

    added search form

--- src/script.js
+++ src/script.js
@@ -50,8 +50,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "0f77edb1aff98694c739634ccdcf1f06";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "0f77edb1aff98694c739634ccdcf1f06";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function controlSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit".controlSubmit);
+
+search("New York");
