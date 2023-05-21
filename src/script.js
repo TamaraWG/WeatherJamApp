11d2ace1521cb178616a7a4b4b55b20681095f4d
TamaraWG <walkertamara32@gmail.com>
2023-05-21 15:24:33 -0500

    update form

--- src/script.js
+++ src/script.js
@@ -61,8 +61,7 @@ function controlSubmit(event) {
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
+search("New York");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit".controlSubmit);
-
-search("New York");
