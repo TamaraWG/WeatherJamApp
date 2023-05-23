f3d12488fdd98ecb442bc7f3c8918c5e3be1f26a
TamaraWG <walkertamara32@gmail.com>
2023-05-23 17:12:22 -0500

    added date data

--- src/script.js
+++ src/script.js
@@ -41,7 +41,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         ` 
-      <div class="col-8">
+      <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
           src="http://openweathermap.org/img/wn/${
